dc142dc8715068602eb8b22d3ab18eacb2376be1
Havenadiel <92788807+Havenadiel@users.noreply.github.com>
2024-11-12 13:51:29 +0000

    Code update

--- Tasksheet129/Gorilla.java
+++ Tasksheet129/Gorilla.java
@@ -3,42 +3,30 @@ interface Animal {
     void groom();
     void pet();
 }
-
 public class Gorilla implements Animal {
 
-    //public static void main (String [] args);
-    private boolean isHungry;
-
-    public Gorilla() {
-        this.isHungry = true;
-    }
-    // put gorilla food into cage
-    @Override
+   @Override
     public boolean feed(boolean timeToEat) {
-        if (timeToEat && isHungry) {
-            System.out.println("The gorilla is being fed.");
-            isHungry = false; 
-            return true;
-        } else if (!timeToEat) {
-            System.out.println("The gorilla is not in the mood to eat right now.");
-            return false;
+        if (timeToEat) {
+            System.out.println("Gorilla is Hungry.");
+            return true;  
         } else {
-            System.out.println("The gorilla is not hungry.");
-            return false;
+            System.out.println("Gorilla is not hungry.");
+            return false;  
         }
     }
     // lather, rinse, repeat
     @Override
     public void groom() {
-        System.out.println("The gorilla is being groomed.");
+        System.out.println("Lather, rinse, repeat");
     }
     // pet at your own risk
     @Override
     public void pet() {
-        System.out.println("You pet the gorilla. It seems to enjoy the attention.");
+        System.out.println("Pet at your own risk");
     }
-
-    public boolean isHungry() {
-        return isHungry;
+    // put gorilla food into cage
+    public void feedInCage() {
+        System.out.println("Put gorilla food into cage");
     }
 }
---
+++ Tasksheet129/mainGorilla.java
@@ -0,0 +1,16 @@
+public class mainGorilla {
+    public static void main(String[] args) {
+        Gorilla gorilla = new Gorilla();
+    
+        gorilla.feed(true);  
+        
+        // Grooming 
+        gorilla.groom();    
+
+        // Petting 
+        gorilla.pet();     
+
+        // Feeding 
+        gorilla.feedInCage(); 
+    }
+}
